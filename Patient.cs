17863d9c57a195c03641b9e15b62239ab18b0324
Amol Shelar <amols2@citiustech.com>
2020-12-08 23:55:46 +0530

    test commit

--- Patient.cs
+++ Patient.cs
@@ -18,7 +18,15 @@ namespace ConsoleDemo
 
         public string Phone { get; set; }
 
-        public string Email { get; set; }
         
+        
+        
+        public string Email { get; set; }
+
+
+
+
+
+
     }
 }
